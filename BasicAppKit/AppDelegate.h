16736c9ca45e5917e677afddf1a0bcdfe749622c
xujun <xujun@lcsw.cn>
2019-08-08 17:32:43 +0800

    Initial Commit

---
+++ BasicAppKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasicAppKit
+//
+//  Created by xujun on 2019/8/8.
+//  Copyright © 2019 xujun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BasicAppKit/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BasicAppKit
+//
+//  Created by xujun on 2019/8/8.
+//  Copyright © 2019 xujun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
